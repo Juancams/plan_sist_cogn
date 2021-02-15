fe31d507d996f5aa7695f2479e0a97d05a250c54
Marina Gil <mgilpensado@gmail.com>
2021-02-15 20:59:36 +0100

    Added comments

--- scan_fake/src/proceso.cpp
+++ scan_fake/src/proceso.cpp
@@ -98,6 +98,7 @@ public:
   explicit ScanFakeSubscriber(const std::string & name)
   : Node(name)
   {
+    // Subscriber
     sub = create_subscription<sensor_msgs::msg::LaserScan>(
       "scan_fake", rclcpp::SensorDataQoS(),
       std::bind(&ScanFakeSubscriber::scan_callback, this, _1));
@@ -106,22 +107,32 @@ public:
 private:
   void scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr scan)
   {
+    // Initialize min and max values 
     min_value = scan->range_max;
     max_value = scan->range_min;
-    float value_sum = 0;
+    
+    float value_sum = 0.0;
+
+    // Loop for checking every value in the scan ranges
     for(int n = 0; n < n_readings; n++)
     {
+      // Compare if it is the maximum value
       if(scan->ranges[n] > max_value)
       {
         max_value = scan->ranges[n];
       }
+      // Compare if it is the minimum value
       else if(scan->ranges[n] < min_value)
       {
         min_value = scan->ranges[n];
       }
+      // Sum of all values
       value_sum += scan->ranges[n];
     }
+    // Result of the mean
     mean = value_sum / n_readings;
+
+    // Show minimum, maximum and mean
     RCLCPP_INFO(get_logger(), 
     "The min value is: [%f] | The max value is: [%f] | The mean is: [%f] ", 
     min_value, max_value, mean);
