11fac1ed2447ccac48b178e19a12ed7839422552
Noel Jimenez Garcia <noelin21@gmail.com>
2021-03-30 15:27:19 +0200

    update launcher

--- house_nav/launch/house_nav_launch.py
+++ house_nav/launch/house_nav_launch.py
@@ -12,23 +12,71 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
 from launch import LaunchDescription
-from launch.actions import SetEnvironmentVariable
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
 
-    stdout_linebuf_envvar = SetEnvironmentVariable('RCUTILS_CONSOLE_STDOUT_LINE_BUFFERED', '1')
+    package_dir = get_package_share_directory('house_nav')
+    namespace = LaunchConfiguration('namespace')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    robots_dir = get_package_share_directory('robots')
 
-    proceso_cmd = Node(
-        package='house_nav',
-        executable='house_nav',
-        output='screen',
-        parameters=[])
+    declare_namespace_cmd = DeclareLaunchArgument(
+        'namespace',
+        default_value='',
+        description='Namespace')
 
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation (Gazebo) clock if true')
+
+    stdout_linebuf_envvar = SetEnvironmentVariable(
+        'RCUTILS_CONSOLE_STDOUT_LINE_BUFFERED', '1')
+
+    plansys2_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(
+            get_package_share_directory('plansys2_bringup'),
+            'launch',
+            'plansys2_bringup_launch_monolithic.py')),
+        launch_arguments={
+          #'model_file': package_dir + '/pddl/domain.pddl',
+          'model_file': '/home/noeljg/Desktop/cognitive-systems-projects/pddl/star_trips_world/star_trips2_domain.pddl',
+          'namespace': namespace
+          }.items())
+
+    webots = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('webots_ros2_core'), 'launch', 'robot_launch.py')
+        ),
+        launch_arguments={
+            'executable': 'webots_differential_drive_node',
+            'world': os.path.join(robots_dir, 'worlds', 'complete_apartment.wbt'),
+            'node_parameters': os.path.join(robots_dir, 'resource', 'tiago.yaml'),
+            'use_sim_time': use_sim_time
+        }.items()
+    )
+
+
+    # Create the launch description
     ld = LaunchDescription()
+    
+    # Set environment variables
     ld.add_action(stdout_linebuf_envvar)
-    ld.add_action(proceso_cmd)
+    ld.add_action(declare_namespace_cmd)
+    ld.add_action(declare_use_sim_time_cmd)
+
+    # Declare the launch options
+    ld.add_action(plansys2_cmd)
+    ld.add_action(webots)
 
     return ld
